448f96f47b7ba6e9e6ef57c46189effa1e97518e
cchho2 <99853667+cchho2@users.noreply.github.com>
2022-11-14 10:49:57 -0500

    Update App.js
    Created homepage/chatpage
    added desgins to header - icons 
    imported routerroute

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import Header from "./Header";
+import TinderCards from './TinderCards'
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Food Swipe !!</h1>
-
-      {/* Header */}
-      <Header />
-      {/* Food Cards */}
-      {/* Buttons below the cards */}
-      {/* Chat screen */}
-      {/* Individual Chat screen */} 
-    </div>
+  <div className="App">
+    <Header /> 
+     <Router>
+       <Switch>
+         <Route path="/chat">
+           <h1>I am the chat  page</h1>
+         </Route>
+         <Route path="/">
+           <TinderCards/>
+         </Route>
+       </Switch>
+     </Router>
+  </div>
   );
 }
 
-export default App;
+export default App; 
+
+{/* <Header /> */}
+
+ {/* Food Cards */}
+   {/* Buttons below the cards */}
+     
+        {/* Chat screen */}
+        {/* Individual Chat screen */} 
+       
+   {/*<h1>Food Swipe !!</h1>*/}
+       
