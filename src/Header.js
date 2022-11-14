bd6692a0d2a29c49205f7342624581f0593a18ba
cchho2 <99853667+cchho2@users.noreply.github.com>
2022-11-14 10:51:57 -0500

    Update Header.js
    added icons to header as well as our logo imported from material ui
    added functionalities from material ui - iconbutton

--- src/Header.js
+++ src/Header.js
@@ -2,14 +2,24 @@ import React from "react";
 import './Header.css';  
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import ForumIcon from '@mui/icons-material/Forum';
+import IconButton from '@mui/material/IconButton';
+
 
 function Header() 
 {
     return (
         <div className="header">
-            <AccountBoxIcon />
-            <h2>I am a header.</h2>
-            <ForumIcon />
+            <IconButton>
+            <AccountBoxIcon className = "header_icon" fontSize="large"/>
+            </IconButton>
+
+            <img className= "header_logo" 
+            src="https://1000logos.net/wp-content/uploads/2022/11/Shopee-Food-Emblem.png"
+             alt="food swipe logo"/>
+
+             <IconButton>
+            <ForumIcon className = "header_icon" fronSize="large"/>
+            </IconButton>
         </div>
     );
 }
