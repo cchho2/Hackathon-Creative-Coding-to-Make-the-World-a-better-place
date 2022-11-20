33bc3e64364f8b968089b6037171de2992d7e7e8
cchho2 <99853667+cchho2@users.noreply.github.com>
2022-11-19 20:00:16 -0500

    Update firebase.js
    Reset firebase and its configs to fix the problem/issue we had

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,19 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAfl6gD0Gx3a_Q8XZ73w-oPc7QVDULeI0Y",
-     authDomain: "tinder-clone-262b4.firebaseapp.com",
-    rojectId: "tinder-clone-262b4",
-    storageBucket: "tinder-clone-262b4.appspot.com",
-    messagingSenderId: "317667358840",
-    appId: "1:317667358840:web:06e6160ddfb7116cc0958d",
-    measurementId: "G-KCMTR78W7S"
-    };
+    apiKey: "AIzaSyDYc4JS-FLzNBDgWfMuCRncwn-9xL0rQiE",
+    authDomain: "food-swipe-2.firebaseapp.com",
+    projectId: "food-swipe-2",
+    storageBucket: "food-swipe-2.appspot.com",
+    messagingSenderId: "146903236710",
+    appId: "1:146903236710:web:7f65326d1d2d70652a6da7",
+    measurementId: "G-LDF3NCM72R"
+  };
     
+
     const firebaseApp = firebase.initializeApp(firebaseConfig); 
     const database = firebaseApp.firestore(); 
 
