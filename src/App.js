b8cfa10f51081d5371fdf797d9e05218132ed3b9
cchho2 <99853667+cchho2@users.noreply.github.com>
2022-11-22 10:56:26 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-import React from 'react';
+import React from "react";
 import Header from "./Header";
-import TinderCards from './TinderCards'
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import TinderCards from "./TinderCards";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SwipeButtons from "./SwipeButtons";
+import Chats from "./Chats";
+import ChatScreen from "./ChatScreen";
 
-import './App.css';
-
-function App() {
+function App() 
+{
   return (
-  <div className="App">
-    <Header /> 
-     <Router>
-       <Switch>
-         <Route path="/chat">
-           <h1>I am the chat  page</h1>
-         </Route>
-         <Route path="/">
-           <TinderCards/>
-         </Route>
-       </Switch>
-     </Router>
-  </div>
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/chat/:person">
+            <Header backButton="/chat" />
+            <ChatScreen/>
+          </Route>
+          <Route path="/chat">
+            <Header backButton="/" />
+            <Chats />
+          </Route>
+          <Route path="/">
+            <Header />
+            <TinderCards />
+            <SwipeButtons />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
-export default App; 
-
-{/* <Header /> */}
-
- {/* Food Cards */}
-   {/* Buttons below the cards */}
-     
-        {/* Chat screen */}
-        {/* Individual Chat screen */} 
-       
-   {/*<h1>Food Swipe !!</h1>*/}
-       
+export default App;
